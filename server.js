93a4761aa8649a531d86a09118fd211204fc75c5
David Cellini <david.cellini@hive.hr>
2021-09-10 14:34:56 +0100

    added all files apart from node modules

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const {ApolloServer} = require('apollo-server-express');
+const {ApiElasticSearchClient} = require('./server.elasticsearch');
+const madeExecutableSchema = require('./server.graphql');
+
+// PORT
+const PORT = 9100;
+
+let apolloServer = null;
+
+async function startServer() {
+    apolloServer = new ApolloServer({
+      schema: madeExecutableSchema,
+      playground: true,
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+}
+
+startServer();
+
+// app.get("/rest", function (req, res) {
+//     res.json({ data: "api working" });
+// });
+
+// app.listen(4000, function () {
+//     console.log(`server running on port 4000`);
+//     console.log(`gql path is ${apolloServer.graphqlPath}`);
+// });
+
+const server = new ApolloServer({
+  schema: madeExecutableSchema,
+  playground: true,
+});
+
+// TODO Use the BodyParser as a middleware
+app.use(bodyParser.json());
+
+// TODO Set port for the app to listen on
+app.set('port', process.env.PORT || 3001);
+
+// TODO Set path to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// TODO Enable CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Access-Control-Request-Method");
+  next();
+});
+
+// Define the `/search` route that should return elastic search results
+app.get('/search', ApiElasticSearchClient);
+
+// server.applyMiddleware({app});
+
+app.listen(PORT, function () {
+  console.log(`Express server listening on port :${PORT}${server.graphqlPath}`);
+});
